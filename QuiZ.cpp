d932ed4add3e31cac72aa1f78c64dddd5abf13c8
laxmikant dhanka <95989532+LaxmikantDhanka@users.noreply.github.com>
2024-08-14 18:22:37 +0530

    Add files via upload

---
+++ QuiZ.cpp
@@ -0,0 +1,40 @@
+//---------------------------------------------------------------------------
+
+#include <fmx.h>
+
+#ifdef _WIN32
+#include <tchar.h>
+#endif
+#pragma hdrstop
+#include <System.StartUpCopy.hpp>
+//---------------------------------------------------------------------------
+USEFORM("StartForm.cpp", MainForm);
+USEFORM("QuizForm.cpp", MyQuizForm);
+//---------------------------------------------------------------------------
+extern "C" int FMXmain()
+{
+	try
+	{
+		Application->Initialize();
+		Application->CreateForm(__classid(TMainForm), &MainForm);
+		Application->CreateForm(__classid(TMyQuizForm), &MyQuizForm);
+		Application->Run();
+	}
+	catch (Exception &exception)
+	{
+		Application->ShowException(&exception);
+	}
+	catch (...)
+	{
+		try
+		{
+			throw Exception("");
+		}
+		catch (Exception &exception)
+		{
+			Application->ShowException(&exception);
+		}
+	}
+	return 0;
+}
+//---------------------------------------------------------------------------
---
+++ QuizForm.cpp
@@ -0,0 +1,118 @@
+//---------------------------------------------------------------------------
+
+#include <fmx.h>
+#include <queue>
+#pragma hdrstop
+
+#include "QuizForm.h"
+//---------------------------------------------------------------------------
+#pragma package(smart_init)
+#pragma resource "*.fmx"
+
+ class Question{
+public:
+	char*Text;
+	char*Answer1;
+	char*Answer2;
+	char*Answer3;
+	int CorrectAnswer;
+	Question(){}
+	Question(char* text,char*ans1,char*ans2,char*ans3,int correctAns){
+	Text = text;
+	Answer1 = ans1;
+	Answer2 = ans2;
+	Answer3 = ans3;
+	int CorrectAnswer = correctAns;
+
+	}
+};
+
+std::queue<Question> LoadQuestions(){
+
+Question q1 = Question(" 1. Which animal is known as the 'Ship of the Deserts ? ","Camel", " Horse","Donkey",1);
+Question q2 = Question(" 2. How many days are there in a week? ","7", "5","6",1);
+Question q3 = Question(" 3. How many hours are there in a day? ","25", " 24","12",2);
+Question q4 = Question(" 4. How many letters are there in the English alphabet? ","22","26","21",2);
+Question q5 = Question(" 5. Rainbow consist of how many colours? ","7", "10","4",1) ;
+std::queue<Question> questions;
+questions.push(q1);
+questions.push(q2);
+questions.push(q3);
+questions.push(q4);
+questions.push(q5);
+
+return questions;
+
+}
+std::queue<Question> questions;
+   Question currentQuestion;
+   int selectedAnswer;
+   int points = 0;
+
+
+
+
+//
+TMyQuizForm *MyQuizForm;
+//---------------------------------------------------------------------------
+__fastcall TMyQuizForm::TMyQuizForm(TComponent* Owner)
+	: TForm(Owner)
+{
+questions = LoadQuestions();
+ currentQuestion =    questions.front();
+ QuestionLabel ->Text = currentQuestion.Text;
+  Answer1RadioButton->Text = currentQuestion.Answer1;
+  Answer2RadioButton->Text = currentQuestion.Answer2;
+  Answer3RadioButton->Text = currentQuestion.Answer3;
+	questions.pop();
+	PointsLabel->Text= points;
+ }
+//---------------------------------------------------------------------------
+void __fastcall TMyQuizForm::Answer1RadioButtonChange(TObject *Sender)
+{
+		selectedAnswer = 1;
+
+}
+//---------------------------------------------------------------------------
+void __fastcall TMyQuizForm::Answer2RadioButtonChange(TObject *Sender)
+{
+	   selectedAnswer = 2;
+}
+//---------------------------------------------------------------------------
+void __fastcall TMyQuizForm::Answer3RadioButtonChange(TObject *Sender)
+{
+	  selectedAnswer = 3;
+}
+//---------------------------------------------------------------------------
+void __fastcall TMyQuizForm::ConfirmButtonClick(TObject *Sender)
+{
+ if(selectedAnswer== currentQuestion.CorrectAnswer)
+		   {
+		   points++ ;
+			}
+
+
+	if(!questions.empty())
+  {
+
+	 PointsLabel->Text= points;
+	 currentQuestion  = questions.front();
+	 QuestionLabel ->Text = currentQuestion.Text;
+	 Answer1RadioButton->Text = currentQuestion.Answer1;
+	 Answer2RadioButton->Text = currentQuestion.Answer2;
+	 Answer3RadioButton->Text = currentQuestion.Answer3;
+		  questions.pop();
+
+  Answer1RadioButton->IsChecked=  false;
+  Answer2RadioButton->IsChecked = false;
+  Answer3RadioButton->IsChecked = false;
+
+  }
+	else
+	{
+	ConfirmButton->Enabled = false;
+	ConfirmButton->Text = " Quiz Finished ";
+	}
+}
+
+//---------------------------------------------------------------------------
